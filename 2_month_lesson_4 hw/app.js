cd7c6f3d7af7fcc5e2c6ae4b640bc456a7809386
Joelwine <stimdlastim@gmail.com>
2024-07-12 16:06:03 +0600

    Add files via upload

---
+++ 2_month_lesson_4 hw/app.js
@@ -0,0 +1,62 @@
+//TODO TEST
+
+// let AutoNumber = prompt("Введите номер машины")
+
+// function validateNumberAuto(AutoNumber) {
+//
+//     if (AutoNumber.length !== 10) return true;
+//
+//     let lastThreeSymbols = AutoNumber.slice(-3)
+//     let noneSymbols = [
+//         "MVD",
+//         "MWD",
+//         "SEX",
+//         "AUE",
+//         "UZB",
+//         "TAJ",
+//         "ORG",
+//         "LOL",
+//         "HUY",
+//         "GAI",
+//     ];
+//     return !noneSymbols.includes(lastThreeSymbols);
+// }
+//
+// console.log(validateNumberAuto("01KG123ABC"));
+// console.log(validateNumberAuto("01KG555MVD"));
+
+// TODO HW
+
+// 2 ЗАДАЧА
+
+
+// function revNumber(Number) {
+//     let revNumber = '';
+//
+//     for (let i = Number.length -1; i >= 0; i--) {
+//         revNumber += Number[i];
+//
+//     }
+//     return revNumber;
+// }
+//
+// console.log(revNumber  (  "123456789"));
+
+
+//1x Задача
+
+let phoneNumber = prompt("Enter your phone number");
+// let phoneNumber = "+996 555 123 123"
+function hideSymbols(Symbols){
+    if (Symbols.length < 12){
+        return Symbols;
+    }
+    return Symbols.slice(0,14) + "xx";
+
+
+}
+let hiddenSymbols = hideSymbols(phoneNumber);
+console.log(phoneNumber + " " + "-->"+ " " + hiddenSymbols);
+
+
+
