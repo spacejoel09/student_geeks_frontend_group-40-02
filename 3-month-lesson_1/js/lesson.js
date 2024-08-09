80864e5fe6213fb90f01d2d1caf7f577d7139314
Joelwine <stimdlastim@gmail.com>
2024-08-09 22:39:42 +0600

    Fight for gold medal in Paris 2024 Olympics!! KG

--- 3-month-lesson_1/js/lesson.js
+++ 3-month-lesson_1/js/lesson.js
@@ -17,3 +17,69 @@ phoneButton.onclick = function () {
 
 
 }
+
+
+const tabContentsBlocks = document.querySelectorAll(".tab_content_block")
+const tabsParent =   document.querySelector(".tab_content_items")
+let index = 0;
+let intervaId;
+
+
+const  tabs = document.querySelectorAll('.tab_content_item');
+const hideTabContent = () => {
+    tabContentsBlocks.forEach((item) => {
+        item.style.display = "none"
+
+    })
+    tabs.forEach(item => {
+        item.classList.remove("tab_content_item_active")
+    })
+}
+
+const showTabContent = (index) => {
+    tabContentsBlocks[index].style.display = "block"
+    tabs[index].classList.add('tab_content_item_active')
+
+
+}
+
+
+
+const startSlide = () => {
+    intervaId = setInterval(() => {
+        index++;
+        if (index > tabs.length - 1) {
+            index = 0;
+        }
+        hideTabContent();
+        showTabContent(index);
+    }, 3000);
+}
+
+
+const resetAutoSlide = () => {
+    clearInterval(intervaId);
+    startSlide();
+};
+
+
+
+
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((item,i) => {
+            if (event.target === item){
+                hideTabContent()
+                index = i
+                showTabContent(i)
+                resetAutoSlide()
+            }
+        })
+    }
+}
+
+
+hideTabContent()
+startSlide()
+showTabContent(0)
